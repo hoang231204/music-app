998c3315dc9213cc6340f680d2f4a36bec0f09ca
hoang231204 <nguyenhoang231204@gmail.com>
2026-07-29 14:29:53 +0700

    feat(topics): add model topics

---
+++ models/topic-model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ITopic extends Document {
+  title?: string;
+  avatar?: string;
+  description?: string;
+  status?: string;
+  slug?: string;
+  deleted: boolean;
+  deletedAt?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const topicSchema = new Schema<ITopic>({
+  title: { type: String },
+  avatar: { type: String },
+  description: { type: String },
+  status: { type: String },
+  slug: { type: String },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  deletedAt: { type: Date },
+}, {
+  timestamps: true,
+});
+
+const Topic = mongoose.model<ITopic>("Topic", topicSchema, "topics");
+export default Topic;
