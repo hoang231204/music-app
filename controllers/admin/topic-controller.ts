26f06e8582b1dc09d77ea8e78445dfd650de7581
hoang231204 <nguyenhoang231204@gmail.com>
2026-07-29 15:14:28 +0700

    feat(topics): add search handing logic to controller

--- controllers/admin/topic-controller.ts
+++ controllers/admin/topic-controller.ts
@@ -1,8 +1,22 @@
 import Topic from "../../models/topic-model";
 import { Request, Response } from "express";
+import createSearchRegex from "../../helpers/search-helper"
+//GET /admin/topics
+type taskStatus = "active" | "inactive";
+interface FindQuery {
+    deleted: boolean;
+    status?: taskStatus;
+    title?: { $regex: RegExp };
+}
 export const index = async (req: Request, res: Response) => {
     try{
-        const topics = await Topic.find({deleted: false});
+        const findQuery: FindQuery = { deleted: false };
+        //search
+         if (req.query.keyword && typeof req.query.keyword === "string") {
+            const regex = createSearchRegex({ keyword: req.query.keyword });
+            findQuery.title = { $regex: regex };
+        }
+        const topics = await Topic.find(findQuery);
         res.render("admin/pages/topics/index", {
             topics: topics
         });
