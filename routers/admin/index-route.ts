1d67db313020cf069b72cf234e94aae2cc47178e
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-04 10:15:34 +0700

    feat(topics): add route GET edit topic

--- routers/admin/index-route.ts
+++ routers/admin/index-route.ts
@@ -1,8 +1,11 @@
 import {Express} from "express";
 import authRouter from "./auth-route";
-
+import topicRouter from "./topic-route";
+import dashboardRouter from "./dashboard-route";
 const route = (app: Express)=>{
     app.use("/admin/auth", authRouter);
+    app.use("/admin/topics", topicRouter);
+    app.use("/admin/dashboard", dashboardRouter);
 }
 
 export default route;
--- routers/admin/topic-route.ts
+++ routers/admin/topic-route.ts
@@ -2,4 +2,5 @@ import {Router} from "express";
 import * as topicController from "../../controllers/admin/topic-controller";
 const router: Router = Router();
 router.get("/", topicController.index);
+router.get("/edit/:id",topicController.edit)
 export default router;
