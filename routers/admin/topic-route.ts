9b022800739947ea4fa2626b70a62c9b5a3a330d
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-04 21:23:22 +0700

    feat(topics): add route patch edit topic

--- routers/admin/topic-route.ts
+++ routers/admin/topic-route.ts
@@ -1,6 +1,10 @@
 import {Router} from "express";
+import multer from "multer"
 import * as topicController from "../../controllers/admin/topic-controller";
+import { upload as uploadCloudinary } from "../../middlewares/admin/upload-cloudinary";
+const upload = multer();
 const router: Router = Router();
 router.get("/", topicController.index);
 router.get("/edit/:id",topicController.edit)
+router.patch("/edit/:id",upload.single("avatar"),uploadCloudinary,topicController.editPatch)
 export default router;
