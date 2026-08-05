52bce8c6d9621755c5faa28614006e7eb53701c0
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-05 14:41:58 +0700

    feat(singers): add controller to get all singers and add filter, search, sort, pagination

---
+++ controllers/admin/singer-controller.ts
@@ -0,0 +1,51 @@
+import Singer from "../../models/singer-model";
+import { Request, Response } from "express";
+import createSearchRegex from "../../helpers/search-helper"
+import filterStatusHelper from "../../helpers/filter-status-helper";
+import paginationHelper from "../../helpers/pagination-helper";
+//GET /admin/singers
+type taskStatus = "active" | "inactive";
+interface FindQuery {
+    deleted: boolean;
+    status?: taskStatus;
+    title?: { $regex: RegExp };
+}
+export const index = async (req: Request, res: Response) => {
+    try{
+        const findQuery: FindQuery = { deleted: false };
+        //filter by status
+        if (req.query.status && typeof req.query.status === "string") {
+            findQuery.status = req.query.status as taskStatus;
+        }
+        const filterStatus = filterStatusHelper(req.query, "singer");
+        //search
+         if (req.query.keyword && typeof req.query.keyword === "string") {
+            const regex = createSearchRegex({ keyword: req.query.keyword });
+            findQuery.title = { $regex: regex };
+        }
+        //sort
+        const sort: Record<string, 1 | -1 | 'asc' | 'desc' | 'ascending' | 'descending'> = {};
+        if (req.query.sortBy && req.query.sortType) {
+            const sortBy = req.query.sortBy as string;
+            const sortType = req.query.sortType as 1 | -1 | 'asc' | 'desc' | 'ascending' | 'descending';
+            sort[sortBy] = sortType;
+        }
+        //pagination
+        const countData: number = await Singer.find(findQuery).countDocuments();
+        const pagination = paginationHelper(req.query, countData);
+        const limit = pagination.limitPage;
+        const skip = pagination.skipPage;
+        const singers = await Singer.find(findQuery).sort(sort).limit(limit).skip(skip);
+        res.render("admin/pages/singers/index", {
+            pageTitle: "Quản lý ca sĩ",
+            singers: singers,
+            keyword: req.query.keyword || "",
+            filterStatus: filterStatus,
+            objectPagination: pagination,
+            countData: countData
+        });
+    }
+    catch(err){
+        console.error(err);
+    }
+}
