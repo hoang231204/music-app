fdc3d56da0088c4c652711fc87d1f6312d096bf2
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-06 10:52:52 +0700

    refactor(helpers): add resource type for upload cloudinary

--- helpers/upload-cloudinary.ts
+++ helpers/upload-cloudinary.ts
@@ -2,14 +2,17 @@ import { v2 as cloudinary, UploadApiResponse, UploadApiErrorResponse } from 'clo
 import { Readable } from 'stream';
 
 const streamUpload = (buffer: Buffer): Promise<UploadApiResponse> => {
-  cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUD_API_KEY, 
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
   });
 
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto",
+      },
       (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
         if (result) {
           resolve(result);
@@ -26,7 +29,6 @@ const streamUpload = (buffer: Buffer): Promise<UploadApiResponse> => {
 // Main function export
 const uploadToCloudinary = async (buffer: Buffer): Promise<string> => {
   const result: UploadApiResponse = await streamUpload(buffer);
-  console.log('Upload result:', result);
   return result.secure_url;
 };
 
