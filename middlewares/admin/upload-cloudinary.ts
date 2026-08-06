c1a594f54b7b000222ae808ef893e91bbabdb123
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-06 10:53:47 +0700

    feat(middleware): add upload field for middleware uploafd cloudinary

--- middlewares/admin/upload-cloudinary.ts
+++ middlewares/admin/upload-cloudinary.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import uploadToCloudinary from '../../helpers/upload-cloudinary';
 
-export const upload = async (
+export const uploadSingle = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -10,6 +10,32 @@ export const upload = async (
     if (req.file) {
       const link = await uploadToCloudinary(req.file.buffer);
       req.body[req.file.fieldname] = link;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const uploadFields = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    if (req.files && typeof req.files === 'object' && !Array.isArray(req.files)) {
+      const filesObject = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+      // Lặp qua từng field (audio, cover, v.v...)
+      for (const fieldname in filesObject) {
+        const fileArray = filesObject[fieldname];
+        if (fileArray && fileArray.length > 0) {
+          const file = fileArray[0];
+          const link = await uploadToCloudinary(file.buffer);
+          req.body[fieldname] = link; // Gán URL vào req.body[fieldname]
+        }
+      }
     }
     next();
   } catch (error) {
