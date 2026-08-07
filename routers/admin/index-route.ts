dc041ad3e9f04cdd2d97187da58a7d59b1885a55
hoang231204 <nguyenhoang231204@gmail.com>
2026-08-07 22:17:52 +0700

    feat: implement route index for admin

--- routers/admin/index-route.ts
+++ routers/admin/index-route.ts
@@ -4,14 +4,19 @@ import topicRouter from "./topic-route";
 import singerRouter from "./singer-route";
 import songRouter from "./song-route";
 import roleRouter from "./role-route";
+import accountRouter from "./account-route";
+import userRouter from "./user-route";
 import dashboardRouter from "./dashboard-route";
+import {requireAuth} from "../../middlewares/admin/require-auth";
 const route = (app: Express)=>{
     app.use("/admin/auth", authRouter);
-    app.use("/admin/topics", topicRouter);
-    app.use("/admin/dashboard", dashboardRouter);
-    app.use("/admin/singers", singerRouter);
-    app.use("/admin/songs", songRouter);
-    app.use("/admin/roles", roleRouter);
+    app.use("/admin/topics", requireAuth, topicRouter);
+    app.use("/admin/dashboard", requireAuth, dashboardRouter);
+    app.use("/admin/singers", requireAuth, singerRouter);
+    app.use("/admin/songs", requireAuth, songRouter);
+    app.use("/admin/roles", requireAuth, roleRouter);
+    app.use("/admin/accounts", requireAuth, accountRouter);
+    app.use("/admin/users", requireAuth, userRouter);
 }
 
 export default route;
